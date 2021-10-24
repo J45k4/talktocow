d881cc5bd8f19f1c9329bee8b389624e42f1d13f
Jaska <pusanteemu@gmail.com>
2021-10-24 12:11:34 +0300

    diary

---
+++ frontend/src/components/diary/diary-entry.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const DiaryEntry = (props: {
+    title: string
+    body: string
+    postedAt: string
+}) => {
+    return (
+        <div>
+            asd
+        </div>
+    )
+}
