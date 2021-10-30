77986141595aeebd1e7aa7560c2c6ef7ad51c4be
Jaska <pusanteemu@gmail.com>
2021-10-30 10:46:03 +0300

    Add build scripts

---
+++ build_push.bat
@@ -0,0 +1,2 @@
+docker build -t jaska/talktocow-backend:0.1.7 .
+docker push jaska/talktocow-backend:0.1.7
---
+++ frontend/build_push.bat
@@ -0,0 +1,2 @@
+docker build -t jaska/talktocow-web:0.0.12 .
+docker push jaska/talktocow-web:0.0.12
