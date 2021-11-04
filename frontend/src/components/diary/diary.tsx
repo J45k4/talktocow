3d9627a4fdab7ba06dfeb48d49c1df3d70a19129
Jaska <pusanteemu@gmail.com>
2021-11-04 20:13:43 +0200

    Add some padding to diary container

--- frontend/src/components/diary/diary.tsx
+++ frontend/src/components/diary/diary.tsx
@@ -10,7 +10,7 @@ export const Diary = () => {
     const router = useRouter()
 
     return (
-        <div>
+        <div className={styles.container}>
             <div className={styles.header}>
                 <Link href="/">
                     <ArrowBack style={{
