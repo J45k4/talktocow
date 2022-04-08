5b68447b2c5479a04fbcf3c063024a407e579278
Jaska <pusanteemu@gmail.com>
2022-04-08 10:19:36 +0300

    Add try catch

--- frontend/src/logic/notification-handler.ts
+++ frontend/src/logic/notification-handler.ts
@@ -2,22 +2,24 @@ import { connEvent } from "./websocket-conn";
 
 export const startNotificationHandler = () => {
     if (Notification) {
-        Notification.requestPermission((status) => {
-            console.log("status", status);
-    
-            if (status === "granted") {
-                connEvent.subscribe({
-                    next: v => {
-                        console.log("v", v);
-    
-                        if (v.type === "messageFromServer") {
-                            if (v.newChatroomMessage) {
-                                new Notification("New message")
+        try {
+            Notification.requestPermission((status) => {
+                console.log("status", status);
+        
+                if (status === "granted") {
+                    connEvent.subscribe({
+                        next: v => {
+                            console.log("v", v);
+        
+                            if (v.type === "messageFromServer") {
+                                if (v.newChatroomMessage) {
+                                    new Notification("New message")
+                                }
                             }
                         }
-                    }
-                })
-            }
-        })
+                    })
+                }
+            })
+        } catch (e) {}
     }
 }
