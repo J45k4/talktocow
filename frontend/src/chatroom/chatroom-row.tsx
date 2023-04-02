52e37a32b596a76dee2e69df2e18b656a4e8e027
Jaska <pusanteemu@gmail.com>
2023-04-02 13:08:15 +0300

    Modify Message row formatting

--- frontend/src/chatroom/chatroom-row.tsx
+++ frontend/src/chatroom/chatroom-row.tsx
@@ -1,7 +1,29 @@
+import { FaReply } from "react-icons/fa"
 import { ChatroomMessage } from "../types"
 import { formatTime } from "../utility"
 
 import styles from "./chatroom-row.module.css"
+
+const MessageTitle = (props: {
+	userName: string
+	writtenAt: string
+}) => {
+	return (
+		<div className={styles.messageTitle}>
+			<div className={styles.messageTitleLeftSide}>
+				<div className={styles.messageAuthor}>
+					{props.userName}
+				</div>
+				<div className={styles.messageTime}>
+					{formatTime(props.writtenAt)}
+				</div>
+			</div>
+			<div className={styles.messageTitleRightSide}>
+				<FaReply />
+			</div>
+		</div>
+	)
+}
 
 export const ChatroomMessageRow = (props: {
 	chatroomMessage: ChatroomMessage
@@ -11,12 +33,10 @@ export const ChatroomMessageRow = (props: {
 		<div className={styles.messageRow} style={{
 			backgroundColor: props.grayBackground ? "#f0f0f0" : "white"
 		}}>
-			<div className={styles.messageAuthor}>
-				{props.chatroomMessage.userName}
-			</div>
-			<div className={styles.messageTime}>
-				{formatTime(props.chatroomMessage.writtenAt)}
-			</div>
+			<MessageTitle
+				userName={props.chatroomMessage.userName}
+				writtenAt={props.chatroomMessage.writtenAt}
+			/>
 			<div>
 				{props.chatroomMessage.messageText}
 			</div>
