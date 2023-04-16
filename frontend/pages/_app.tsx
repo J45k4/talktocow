31f70a2e007fa3b2d157913a2fff55994d5b5991
Jaska <pusanteemu@gmail.com>
2023-04-16 09:56:32 +0300

    Remove emoji

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App({ Component, pageProps }: { Component: any, pageProp
     return (
         <Fragment>
             <Head>
-                <title>Talktocow 🥰 </title>
+                <title>Talktocow</title>
 				<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, width=device-width, user-scalable=no" />
             </Head>
             <Component {...pageProps} />
