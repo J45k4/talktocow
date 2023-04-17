33244777659778217c466ee81294ef23817709ee
Jaska <pusanteemu@gmail.com>
2023-04-17 11:21:13 +0300

    Modify which chatrooms come first when fething my chatrooms

--- routes/chatrooms.go
+++ routes/chatrooms.go
@@ -42,11 +42,11 @@ func GetMyChatrooms(
 
 	// Find all distinct chatrooms where the user is a member and order them by the last message
 	chatrooms, err := models.Chatrooms(
+		qm.Select("chatrooms.*, GREATEST(COALESCE(latest_messages.latest_message, '1970-01-01'), chatrooms.created_at) as max_date"),
 		qm.InnerJoin("chatroom_users on chatroom_users.chatroom_id = chatrooms.id"),
 		qm.Where("chatroom_users.user_id = ?", userSession.UserID),
 		qm.LeftOuterJoin("(SELECT chatroom_id, MAX(created_at) AS latest_message FROM messages GROUP BY chatroom_id) AS latest_messages ON latest_messages.chatroom_id = chatrooms.id"),
-		qm.LeftOuterJoin("messages ON messages.chatroom_id = chatrooms.id AND messages.created_at = latest_messages.latest_message"),
-		qm.OrderBy("COALESCE(latest_messages.latest_message, '1970-01-01') DESC"),
+		qm.OrderBy("max_date DESC"),
 	).All(ctx, db)
 
 	if err != nil {
